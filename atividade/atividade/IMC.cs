693e5323329577ab3dc79a750fadcbc2c268f793
higorabreu20 <37940613+higorabreu20@users.noreply.github.com>
2018-10-16 20:28:34 -0300

    Add files via upload

--- atividade/atividade/IMC.cs
+++ atividade/atividade/IMC.cs
@@ -8,5 +8,31 @@ namespace atividade
 {
     class IMC
     {
+        private double peso;
+        private double altura;
+
+        public IMC()
+        {
+            this.peso = 0.0;
+            this.altura = 0.0;
+        }
+        public IMC(double peso, double altura)
+        {
+            this.peso = peso;
+            this.altura = altura;
+        }
+
+        public double Calcular(double peso, double altura)
+        {
+            return peso / (altura * altura);
+        }
+        
+        public double Classificar(double imc)
+        {
+            return 0.0;
+
+        }
+           
+        }
     }
 }
