81309ea6e37396e33f43f36134e27504037b692b
higorabreu20 <37940613+higorabreu20@users.noreply.github.com>
2018-10-16 20:27:08 -0300

    Add files via upload

---
+++ atividade/atividade/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atividade
+{
+    class Program
+    {
+        static int Maior(int a, int b)
+        {
+            if (a > b)
+            {
+                return a;
+            }
+            else
+            {
+                return b; 
+            }
+        }
+
+        static int Maior2(int a, int b)
+        {
+            return (a > b) ? a : b;
+
+        }
+            static bool Diferente(int x)
+        {
+            if (x % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+          
+
+           
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Maior(1,2));
+            Console.WriteLine(Maior2(3, 4));
+            Console.ReadKey();
+
+            Console.WriteLine(Diferente(5));
+            Console.ReadKey();
+
+
+
+
+
+        }
+    }
+}
